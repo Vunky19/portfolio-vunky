aa35c223ff56f20625871baf5f3d957906990a3d
Vunky Himawan <vunkyhimawan19@gmail.com>
2023-07-09 00:56:56 +0700

    intro not done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let intro = document.querySelector('.intro');
+const intro = document.querySelector('.intro');
 const aboutSec = document.querySelector('.about');
 const skills = document.querySelector('.skills');
 const nav = document.querySelector('.autohide');
@@ -6,16 +6,19 @@ const list = document.querySelectorAll('.icon');
 const project = document.querySelector('.project');
 const blog = document.querySelector('.blog');
 const boxBlog = document.querySelectorAll('.box-blog');
+const kotak1 = document.querySelector('.kotak1');
+const kotak2 = document.querySelector('.kotak2');
+const kotak3 = document.querySelector('.kotak3');
+const kotak4 = document.querySelector('.kotak4');
+const kotak5 = document.querySelector('.kotak5');
+const kotak6 = document.querySelector('.kotak6');
+const bdy = document.querySelector('.body');
+const cnt2 = document.querySelector('.box');
+const landing = document.querySelector('.landing');
+const zeroPad = (num, places) => String(num).padStart(places, '0');
+let x = 0;
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  setTimeout(() => {
-    intro.classList.replace('top-0', 'top-[-100vh]')
-  }, 4200)
-
-})
-
-window.onscroll = function (event) {
+window.onscroll = function () {
   let scroll = window.scrollY;
   let docHeight = document.body.offsetHeight;
   let winHeight = window.innerHeight;
@@ -52,15 +55,15 @@ window.onscroll = function (event) {
     project.classList.remove('bg-black', 'text-white');
   }
   if (scrollPercentRounded >= 90 && scrollPercentRounded <= 100) {
+    boxBlog.forEach((e) => {
+      e.classList.replace('bg-[#161616]', 'bg-slate-200');
+    })
     blog.classList.remove('bg-black', 'text-white');
-    boxBlog.forEach((e) => {
-      e.classList.replace('bg-[#161616]', 'bg-gray-200');
-    })
   } else {
-    blog.classList.add('bg-black', 'text-white');
     boxBlog.forEach((e) => {
-      e.classList.replace('bg-gray-200', 'bg-[#161616]');
+      e.classList.replace('bg-slate-200', 'bg-[#161616]');
     })
+    blog.classList.add('bg-black', 'text-white');
   }
 }
 
@@ -76,6 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let winHeight = window.innerHeight;
       let scrollPercent = scroll_top / (docHeight - winHeight);
       let scrollPercentRounded = Math.round(scrollPercent * 100);
+
       if (scroll_top < last_scroll_top) {
         if (scrollPercentRounded >= 15 && scrollPercentRounded <= 34) {
           el_autohide.classList.remove('scrolled-down');
@@ -100,4 +104,73 @@ document.addEventListener("DOMContentLoaded", function () {
       last_scroll_top = scroll_top;
     });
   }
-}); 
+});
+
+$(document).ready(function () {
+  $(this).scrollTop(0)
+})
+
+
+window.addEventListener('wheel', (event) => {
+  if (intro.classList.contains('flex')) {
+    let scroll = window.scrollY;
+    let scrollY = event.deltaY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scroll / (docHeight - winHeight);
+    let scrollPercentRounded = Math.round(scrollPercent * 100);
+    const el_autohide = document.querySelector('.autohide');
+
+    console.log(scrollPercentRounded)
+
+    if (scrollY > 0) {
+      x = (0 + scrollPercentRounded);
+      if (scrollPercentRounded >= 0 && scrollPercentRounded <= 100) {
+        if (scrollPercentRounded === 100) {
+          bdy.classList.remove('h-[1000vw]', 'w-full');
+          bdy.classList.add('overflow-hidden');
+          
+          setTimeout(() => {
+            $("html, body").scrollTop(0);
+          }, 200)
+          
+          cnt2.classList.replace('absolute', 'hidden');
+          setTimeout(() => {
+            landing.classList.remove('fixed', 'z-40');
+            bdy.classList.remove('overflow-hidden');
+            aboutSec.classList.remove('hidden');
+            skills.classList.remove('hidden');
+            project.classList.remove('hidden');
+            blog.classList.remove('hidden');
+            kotak1.style.transform = `translateY(-100%)`;
+            kotak2.style.transform = `translateY(100%)`;
+            el_autohide.classList.add('scrolled-up');
+          }, 1000);
+
+          setTimeout(() => {
+            intro.classList.replace('flex', 'hidden');
+          }, 2000)
+        } else if (scrollPercentRounded === 0) {
+          kotak5.style.transform = `translateX(${(x)}%)`;
+          kotak6.style.transform = `translateX(-${(x)}%)`;
+        } else {
+          kotak5.style.transform = `translateX(${(x)}%)`;
+          kotak6.style.transform = `translateX(-${(x)}%)`;
+        }
+      }
+    } else if (scrollY < 0) {
+      console.log(x)
+      if (x >= 1) {
+        x = (0 + scrollPercentRounded - 1);
+      }
+      if (scrollPercentRounded > 0 && scrollPercentRounded < 100) {
+        scrollPercentRounded--;
+        kotak5.style.transform = `translateX(${(x)}%)`;
+        kotak6.style.transform = `translateX(-${(x)}%)`;
+      } else if (scrollPercentRounded === 0) {
+        kotak5.style.transform = `translateX(${(x)}%)`;
+        kotak6.style.transform = `translateX(-${(x)}%)`;
+      }
+    }
+  }
+});
