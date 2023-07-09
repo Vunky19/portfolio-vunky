700fcf6f1c104c3fee08ca28ce849946d52d0718
Vunky Himawan <vunkyhimawan19@gmail.com>
2023-07-09 16:27:45 +0700

    update v-1.3

--- app.js
+++ app.js
@@ -15,10 +15,22 @@ const kotak6 = document.querySelector('.kotak6');
 const bdy = document.querySelector('.body');
 const cnt2 = document.querySelector('.box');
 const landing = document.querySelector('.landing');
+const menu = document.querySelector('.menu');
+const backdownMenu = document.querySelector('.backdown-menu');
+const hamburger = document.querySelector('.hamburger-menu')
+const xMenu = document.querySelector('.x-menu')
+const el_autohide = document.querySelector('.autohide');
 const zeroPad = (num, places) => String(num).padStart(places, '0');
 let x = 0;
 
-window.onscroll = function () {
+menu.addEventListener('click', () => {
+  backdownMenu.classList.toggle('translate-y-[-120%]');
+  backdownMenu.classList.toggle('translate-y-0');
+  hamburger.classList.toggle('hidden');
+  xMenu.classList.toggle('hidden');
+})
+
+/* window.onscroll = function () {
   let scroll = window.scrollY;
   let docHeight = document.body.offsetHeight;
   let winHeight = window.innerHeight;
@@ -29,7 +41,6 @@ window.onscroll = function () {
 
   if (scrollPercentRounded >= 15 && scrollPercentRounded <= 34) {
     aboutSec.classList.replace('bg-white', 'bg-black');
-    aboutSec.classList.add('text-white');
   } else {
     aboutSec.classList.replace('bg-black', 'bg-white')
     aboutSec.classList.remove('text-white')
@@ -65,11 +76,10 @@ window.onscroll = function () {
     })
     blog.classList.add('bg-black', 'text-white');
   }
-}
+} */
 
 document.addEventListener("DOMContentLoaded", function () {
 
-  let el_autohide = document.querySelector('.autohide');
 
   if (el_autohide) {
     var last_scroll_top = 0;
@@ -80,28 +90,30 @@ document.addEventListener("DOMContentLoaded", function () {
       let scrollPercent = scroll_top / (docHeight - winHeight);
       let scrollPercentRounded = Math.round(scrollPercent * 100);
 
-      if (scroll_top < last_scroll_top) {
-        if (scrollPercentRounded >= 15 && scrollPercentRounded <= 34) {
-          el_autohide.classList.remove('scrolled-down');
-          el_autohide.classList.add('scrolled-up', 'text-white');
-        } else if (scrollPercentRounded >= 42 && scrollPercentRounded <= 63) {
-          el_autohide.classList.remove('scrolled-down', 'text-white');
-          el_autohide.classList.add('scrolled-up');
-        } else if (scrollPercentRounded >= 68 && scrollPercentRounded <= 87) {
-          el_autohide.classList.remove('scrolled-down');
-          el_autohide.classList.add('scrolled-up', 'text-white');
-        } else if (scrollPercentRounded >= 90 && scrollPercentRounded <= 100) {
-          el_autohide.classList.remove('scrolled-down', 'text-white');
-          el_autohide.classList.add('scrolled-up');
+      if (!backdownMenu.classList.contains('translate-y-0')) {
+        if (scroll_top < last_scroll_top) {
+          if (scrollPercentRounded >= 15 && scrollPercentRounded <= 34) {
+            el_autohide.classList.remove('scrolled-down');
+            el_autohide.classList.add('scrolled-up');
+          } else if (scrollPercentRounded >= 42 && scrollPercentRounded <= 63) {
+            el_autohide.classList.remove('scrolled-down');
+            el_autohide.classList.add('scrolled-up');
+          } else if (scrollPercentRounded >= 68 && scrollPercentRounded <= 87) {
+            el_autohide.classList.remove('scrolled-down');
+            el_autohide.classList.add('scrolled-up');
+          } else if (scrollPercentRounded >= 90 && scrollPercentRounded <= 100) {
+            el_autohide.classList.remove('scrolled-down');
+            el_autohide.classList.add('scrolled-up');
+          } else {
+            el_autohide.classList.remove('scrolled-down');
+            el_autohide.classList.add('scrolled-up');
+          }
         } else {
-          el_autohide.classList.remove('scrolled-down', 'text-white');
-          el_autohide.classList.add('scrolled-up');
+          el_autohide.classList.remove('scrolled-up');
+          el_autohide.classList.add('scrolled-down');
         }
-      } else {
-        el_autohide.classList.remove('scrolled-up');
-        el_autohide.classList.add('scrolled-down');
+        last_scroll_top = scroll_top;
       }
-      last_scroll_top = scroll_top;
     });
   }
 });
@@ -119,7 +131,6 @@ window.addEventListener('wheel', (event) => {
     let winHeight = window.innerHeight;
     let scrollPercent = scroll / (docHeight - winHeight);
     let scrollPercentRounded = Math.round(scrollPercent * 100);
-    const el_autohide = document.querySelector('.autohide');
 
     console.log(scrollPercentRounded)
 
@@ -129,11 +140,11 @@ window.addEventListener('wheel', (event) => {
         if (scrollPercentRounded === 100) {
           bdy.classList.remove('h-[1000vw]', 'w-full');
           bdy.classList.add('overflow-hidden');
-          
+
           setTimeout(() => {
             $("html, body").scrollTop(0);
           }, 200)
-          
+
           cnt2.classList.replace('absolute', 'hidden');
           setTimeout(() => {
             landing.classList.remove('fixed', 'z-40');
