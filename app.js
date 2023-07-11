ddc8b4c221ad1277fef6400066f5a5c252928b74
Vunky Himawan <vunkyhimawan19@gmail.com>
2023-07-11 14:33:28 +0700

    update version-1.10

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const intro = document.querySelector('.intro');
-const aboutSec = document.querySelector('.about');
-const skills = document.querySelector('.skills');
+const btn = document.querySelector('.btn');
+const plane = document.querySelector('.fly')
+const intro_el = document.querySelector('.intro');
+const introNum_el = document.querySelector('.intro-num');
+const introBox_el = document.querySelector('.box');
 const nav = document.querySelector('.autohide');
-const list = document.querySelectorAll('.icon');
-const project = document.querySelector('.project');
-const blog = document.querySelector('.blog');
-const boxBlog = document.querySelectorAll('.box-blog');
-const kotak1 = document.querySelector('.kotak1');
-const kotak2 = document.querySelector('.kotak2');
-const kotak3 = document.querySelector('.kotak3');
-const kotak4 = document.querySelector('.kotak4');
-const kotak5 = document.querySelector('.kotak5');
-const kotak6 = document.querySelector('.kotak6');
-const bdy = document.querySelector('.body');
-const cnt2 = document.querySelector('.box');
-const landing = document.querySelector('.landing');
 const menu = document.querySelector('.menu');
 const backdownMenu = document.querySelector('.backdown-menu');
 const hamburger = document.querySelector('.hamburger-menu')
@@ -22,6 +11,27 @@ const xMenu = document.querySelector('.x-menu')
 const el_autohide = document.querySelector('.autohide');
 const zeroPad = (num, places) => String(num).padStart(places, '0');
 let x = 0;
+
+btn.addEventListener('click', () => {
+  plane.classList.toggle('translate-y-32');
+})
+
+/* window.addEventListener('DOMContentLoaded', () => {
+  let count = -100;
+  const counter = setInterval(() => {
+    x++;
+    count = (-100 + x);
+    introBox_el.classList.replace(`translate-x-[${count - 1}%]`, `translate-x-[${count}%]`);
+    introNum_el.textContent = zeroPad(x, 3);
+    if (x === 100) {
+      clearInterval(counter);
+      intro_el.classList.add('scale-150', 'opacity-0');
+      setTimeout(() => {
+        intro_el.classList.add('hidden');
+      }, 1000)
+    }
+  }, 100);
+}) */
 
 menu.addEventListener('click', () => {
   backdownMenu.classList.toggle('translate-y-[-120%]');
@@ -55,84 +65,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-
-/* $(document).ready(function () {
-  $(this).scrollTop(0)
-})
-
-if (window.innerWidth >= 1024) {
-
-  document.addEventListener('wheel', (event) => {
-    if (intro.classList.contains('flex')) {
-      let scroll = window.scrollY;
-      let scrollY = event.deltaY;
-      let docHeight = document.body.offsetHeight;
-      let winHeight = window.innerHeight;
-      let scrollPercent = scroll / (docHeight - winHeight);
-      let scrollPercentRounded = Math.round(scrollPercent * 100);
-
-      document.title = scrollPercentRounded;
-
-      if (scrollY > 0) {
-        x = (0 + scrollPercentRounded);
-        if (scrollPercentRounded >= 0 && scrollPercentRounded <= 100) {
-          if (scrollPercentRounded === 100) {
-            bdy.classList.remove('h-[1000vw]', 'w-full');
-            bdy.classList.add('overflow-hidden');
-
-            setTimeout(() => {
-              $("html, body").scrollTop(0);
-            }, 200)
-
-            cnt2.classList.replace('absolute', 'hidden');
-            setTimeout(() => {
-              landing.classList.remove('fixed', 'z-40');
-              bdy.classList.remove('overflow-hidden');
-              aboutSec.classList.remove('hidden');
-              skills.classList.remove('hidden');
-              project.classList.remove('hidden');
-              blog.classList.remove('hidden');
-              kotak1.style.transform = `translateY(-100%)`;
-              kotak2.style.transform = `translateY(100%)`;
-              el_autohide.classList.add('scrolled-up');
-            }, 1000);
-
-            setTimeout(() => {
-              intro.classList.replace('flex', 'hidden');
-            }, 2000)
-          } else if (scrollPercentRounded === 0) {
-            kotak5.style.transform = `translateX(${(x)}%)`;
-            kotak6.style.transform = `translateX(-${(x)}%)`;
-          } else {
-            document.title = 'Oke';
-            kotak5.style.transform = `translateX(${(x)}%)`;
-            kotak6.style.transform = `translateX(-${(x)}%)`;
-          }
-        }
-      } else if (scrollY < 0) {
-        console.log(x)
-        if (x >= 1) {
-          x = (0 + scrollPercentRounded - 1);
-        }
-        if (scrollPercentRounded > 0 && scrollPercentRounded < 100) {
-          scrollPercentRounded--;
-          kotak5.style.transform = `translateX(${(x)}%)`;
-          kotak6.style.transform = `translateX(-${(x)}%)`;
-        } else if (scrollPercentRounded === 0) {
-          kotak5.style.transform = `translateX(${(x)}%)`;
-          kotak6.style.transform = `translateX(-${(x)}%)`;
-        }
-      }
-    }
-  });
-} else {
-  $("html, body").scrollTop(0);
-  intro.classList.replace('flex', 'hidden');
-  bdy.classList.remove('h-[1000vw]', 'w-full');
-  cnt2.classList.replace('absolute', 'hidden');
-  aboutSec.classList.remove('hidden');
-   skills.classList.remove('hidden'); 
-   project.classList.remove('hidden'); 
-  blog.classList.remove('hidden');
-  landing.classList.remove('fixed', 'z-40');
-} */
